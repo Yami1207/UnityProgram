c4b347e46edfd06108d305359ebbe9ccbccd0538
Yami1207 <xingshuang1984@qq.com>
2021-12-21 23:42:26 +0800

    upload scripts

---
+++ VisualEffect/CloudShadow/Assets/Scripts/CloudShadowSystem.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CloudShadowSystem : MonoBehaviour
+{
+    /// <summary>
+    /// Shader相关属性
+    /// </summary>
+    private readonly static int PROPERTY_CLOUD_SHADOW_TEXTURE = Shader.PropertyToID("g_CloudShadowTexture");
+    private readonly static int PROPERTY_CLOUD_SHADOW_COLOR = Shader.PropertyToID("g_CloudShadowColor");
+    private readonly static int PROPERTY_CLOUD_SHADOW_VELOCITY = Shader.PropertyToID("g_CloudShadowVelocity");
+    private readonly static int PROPERTY_CLOUD_SHADOW_TILE = Shader.PropertyToID("g_CloudShadowTile");
+
+    /// <summary>
+    /// 纹理UV平铺值
+    /// </summary>
+    [SerializeField]
+    private Vector2 m_TileUV = new Vector2(120.0f, 80.0f);
+    public Vector3 TileUV
+    {
+        set
+        {
+            m_TileUV = value;
+            Shader.SetGlobalVector(PROPERTY_CLOUD_SHADOW_TILE, new Vector4(1.0f / m_TileUV.x, 1.0f / m_TileUV.y, 1.0f, 1.0f));
+        }
+        get { return m_TileUV; }
+    }
+
+    /// <summary>
+    /// UV移动方向
+    /// </summary>
+    [SerializeField]
+    private Vector3 m_Direction = new Vector3(1.0f, 0.0f, 1.0f);
+    public Vector3 Direction
+    {
+        set
+        {
+            m_Direction = value;
+            Shader.SetGlobalVector(PROPERTY_CLOUD_SHADOW_VELOCITY, m_Speed * Vector3.Normalize(m_Direction));
+        }
+        get { return m_Direction; }
+    }
+
+    /// <summary>
+    /// 云影移动速度
+    /// </summary>
+    [SerializeField]
+    private float m_Speed = 0.05f;
+    public float Speed
+    {
+        set
+        {
+            m_Speed = value;
+            Shader.SetGlobalVector(PROPERTY_CLOUD_SHADOW_VELOCITY, m_Speed * Vector3.Normalize(m_Direction));
+        }
+        get { return m_Speed; }
+    }
+
+    /// <summary>
+    /// 云影颜色值
+    /// </summary>
+    [SerializeField]
+    private Color m_Color = new Color(0.0f, 0.0f, 0.0f, 0.48f);
+    public Color Color
+    {
+        set
+        {
+            m_Color = value;
+            Shader.SetGlobalColor(PROPERTY_CLOUD_SHADOW_COLOR, m_Color);
+        }
+        get { return m_Color; }
+    }
+
+    /// <summary>
+    /// 云纹理
+    /// </summary>
+    [SerializeField]
+    private Texture2D m_Texture = null;
+    public Texture2D CloudTexture
+    {
+        set
+        {
+            m_Texture = value;
+            Shader.SetGlobalTexture(PROPERTY_CLOUD_SHADOW_TEXTURE, m_Texture);
+        }
+        get { return m_Texture; }
+    }
+
+    private void OnEnable()
+    {
+        this.SetGlobalAttributes();
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        this.SetGlobalAttributes();
+    }
+#endif
+
+    /// <summary>
+    /// 设置云影相关参数
+    /// </summary>
+    private void SetGlobalAttributes()
+    {
+        Shader.SetGlobalTexture(PROPERTY_CLOUD_SHADOW_TEXTURE, m_Texture);
+        Shader.SetGlobalColor(PROPERTY_CLOUD_SHADOW_COLOR, m_Color);
+        Shader.SetGlobalVector(PROPERTY_CLOUD_SHADOW_VELOCITY, m_Speed * Vector3.Normalize(m_Direction));
+        Shader.SetGlobalVector(PROPERTY_CLOUD_SHADOW_TILE, new Vector4(1.0f / m_TileUV.x, 1.0f / m_TileUV.y, 1.0f, 1.0f));
+    }
+}
---
+++ VisualEffect/CloudShadow/Assets/Shader/CloudShadow.cginc
@@ -0,0 +1,39 @@
+#ifndef __CLOUD_SHADOW_CGINC__
+#define __CLOUD_SHADOW_CGINC__
+
+#if ENABLE_CLOUD_SHADOW
+
+// 云影UV平铺值
+uniform float2 g_CloudShadowTile;
+
+// 云影速度
+uniform float3 g_CloudShadowVelocity;
+
+// 云影颜色值
+uniform half4 g_CloudShadowColor;
+
+// 云影纹理
+uniform sampler2D g_CloudShadowTexture;
+
+// 计算云影值
+inline half3 MixCloudShadow(half3 color, float3 positionWS)
+{
+	//float2 uv = (1 + world_pos.xz * _CloudShadowOneOverTile.xy) * 0.5;
+	//uv.y = 1 - uv.y;
+
+	// 根据像素世界坐标计算UV值
+	float2 uv = positionWS.xz * g_CloudShadowTile.xy - _Time.y * g_CloudShadowVelocity.xz;
+
+	// 云影厚度
+	half thickness = tex2D(g_CloudShadowTexture, uv).r * g_CloudShadowColor.a;
+
+	// 混合当前像素和云影
+	return color * lerp(1.0, g_CloudShadowColor.rgb, thickness);
+}
+
+#define MIX_CLOUD_SHADOW(color, positionWS)	MixCloudShadow(color, positionWS)
+#else
+#define MIX_CLOUD_SHADOW(color, positionWS) 
+#endif
+
+#endif

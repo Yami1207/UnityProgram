d228814dc8c1a847d70d63767961acb1a280b1c2
Yami1207 <56525140+Yami1207@users.noreply.github.com>
2021-12-24 16:39:32 +0800

    提交代码

---
+++ VisualEffect/Painting/Assets/Scripts/DecalPaint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Painting
+{
+    public class DecalPaint : MonoBehaviour, PaintInterface
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #region PaintInterface
+
+        public void Tick(float time)
+        {
+
+        }
+
+        public void Create(Vector3 pos, Quaternion rotation, Vector3 forward, System.Action<GameObject> callback)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        #endregion
+    }
+}
